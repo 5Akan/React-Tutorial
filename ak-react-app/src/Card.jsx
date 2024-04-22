cc93ff1d712ac65ddd458eaec5e7633ed6c2bdca
5Akan <akanimohsobot@gmail.com>
2024-04-22 14:18:47 +0100

    Card components

--- ak-react-app/src/Card.jsx
+++ ak-react-app/src/Card.jsx
@@ -3,9 +3,9 @@ import ProfilePic from './assets/pro.png'
 function Card(){
     return(
         <div className="card">
-            <img src = {ProfilePic} alt="profile-picture" />
-            <h2>Akanimoh</h2>
-            <p>A student of computer Engineering and love anime</p>
+            <img className = "card-img" src = {ProfilePic} alt="profile-picture" />
+            <h2 className = "card-title">Akanimoh</h2>
+            <p className = "card-text">A student of computer Engineering and loves anime</p>
 
         </div>
     )
