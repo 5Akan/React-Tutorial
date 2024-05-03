a3046392a7c560e74e1e5571a9dbf956c2f129fa
5Akan <akanimohsobot@gmail.com>
2024-05-03 17:57:29 +0100

    Default Prop

--- ak-react-app/src/App.jsx
+++ ak-react-app/src/App.jsx
@@ -4,14 +4,16 @@
 //PropTypes =  a mechanism that ensures that the passed value
 //             is of the correct datatype
 //            age:PropTypes.numbers
+//Default Props
 import Student from './Student.jsx'
 function App() {
   return(
     <>
-  <Student name="Spongebob" age = '13' isStudent = {true}/>
+  <Student name="Spongebob" age = {29} isStudent = {true}/>
   <Student name = "Patrick" age = {18} isStudent  = {false}/>
   <Student name = "Squidward" age = {50} isStudent  = {false}/>
   <Student name = "Sandy" age = {22} isStudent  = {true}/>
+  <Student name = "Akanimoh"/>
   </>
   )
 }
