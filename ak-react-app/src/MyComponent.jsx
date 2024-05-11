01543929c9c3dc34125736cf606896dd32a46caf
5Akan <akanimohsobot@gmail.com>
2024-05-11 23:08:19 +0100

    More examples on useState (React Hook)

--- ak-react-app/src/MyComponent.jsx
+++ ak-react-app/src/MyComponent.jsx
@@ -1,13 +1,29 @@
 import React, {useState} from 'react'
 function MyComponent(){
 const [name,setName] = useState("Guest"); //Array destructuring
+const [age,setAge] = useState(9);
+const [isEmployed,setIsEmployed] = useState(false);
+
 const updateName = () => {
-    setName("Spongebob")
+    setName("Spongebob");
 }
+const updateAge = () => {
+setAge(age + 1)
+}
+const updateEmployment = () =>{
+    setIsEmployed(true);
+}
+
 return (
+
     <div>
         <p>Name: {name}</p>
-        <button onClick={updateName}>Set Name</button>
+        <p>Age: {age}</p>
+        <button onClick = {updateName}>Set Name</button>
+        <button onClick={updateAge}>Set Age</button>
+
+        <p>Employment Status: {isEmployed?"Yes":"No"}</p>
+        <button onClick={updateEmployment}>Update Employment</button>
 
     </div>
 )
