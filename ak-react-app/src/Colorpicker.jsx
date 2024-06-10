2652c1055e436b51861484e6a375de2fcd6f6085
5Akan <akanimohsobot@gmail.com>
2024-06-10 20:18:01 +0100

    Added input its type and value and also used css in javascipt

--- ak-react-app/src/Colorpicker.jsx
+++ ak-react-app/src/Colorpicker.jsx
@@ -1,24 +1,40 @@
 //Color Picker needs
 //Area for selecting the color
-    //html-
+    //Have input tag
+    //the input should possesses a type for color
+    //the input should value of color for changing the color
+    //html-input holds the onChange event to the function holding the setter fn
+        // setter fn displays the value of color in the DOM
+    
 
 //Area that displays the color
     //html-div area that contains the stateful variable displaying
-        //colors that changes with onChange
+    //div should have style = backgroundColor:color,
+        //it indicates that the background color will be affected by
+        //the color variable
+        
 
 
 import React,{useState} from "react"
 function colorPicker(params) {
-    const {color, setColor} = useState("#ffffff");
+    const [color, setColor] = useState("#ffffff");
+
+    function handleColor(event) {
+        setColor(event.target.value)
+    }
+
     return(
         <>
         <div className="color-picker-container">
             <h1>Color Picker</h1>
-            <div className="color-display" style={{backgroundColor:color}}></div>
-    
-        
-            
+            <div className="color-display" style={{backgroundColor:color}}>
+                
+                <p>Selected Colour: {color}</p>
+            </div>        
+            <label>Select a Colour</label>
+            <input type="color" value={color} onChange = {handleColor}/>
         </div>
+        
         </>
     )
 }
